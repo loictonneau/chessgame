1debd6f7e50b2ce963968c06c4b644ebc74c013e
PRENOM NOM <EMAIL@DOMAINE>
2022-10-08 19:24:43 +0200

    changement du nom d'une variable

--- chess_game/rook.py
+++ chess_game/rook.py
@@ -6,8 +6,8 @@ class Rook(pieces.Piece):
     def __init__(self, piece_type, color, position):
         super().__init__(piece_type, color, position)
 
-    def get_available_move(self, list_pieces):  # creer une liste avec tout les coups possibles de la tours
-        self.clear_available_move()
+    def get_available_moves(self, list_pieces):  # creer une liste avec tout les coups possibles de la tours
+        self.clear_available_moves()
         row = self.position[0]
         column = self.position[1]
 
@@ -15,9 +15,9 @@ class Rook(pieces.Piece):
             for row_down in range(row + 1, constants.row):
                 piece = pieces.find_piece(list_pieces, row_down, column)
                 if piece.type is None:
-                    self.available_move.append([row_down, column])
+                    self.available_moves.append([row_down, column])
                 elif self.color is not piece.color:# ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
-                    self.available_move.append([row_down, column])
+                    self.available_moves.append([row_down, column])
                     break
                 elif self.color is piece.color :
                     break
@@ -26,9 +26,9 @@ class Rook(pieces.Piece):
             for row_up in range(row - 1, -1, -1):
                 piece = pieces.find_piece(list_pieces, row_up, column)
                 if piece.type is None:
-                    self.available_move.append([row_up, column])
+                    self.available_moves.append([row_up, column])
                 elif self.color is not piece.color:# ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
-                    self.available_move.append([row_up, column])
+                    self.available_moves.append([row_up, column])
                     break
                 elif self.color is piece.color:
                     break
@@ -38,9 +38,9 @@ class Rook(pieces.Piece):
                 piece = pieces.find_piece(list_pieces, row, column_right)
                 print (piece.position)
                 if piece.type is None:
-                    self.available_move.append([row, column_right])
+                    self.available_moves.append([row, column_right])
                 elif self.color is not piece.color:  # ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
-                    self.available_move.append([row, column_right])
+                    self.available_moves.append([row, column_right])
                     break
                 elif self.color is piece.color:
                     break
@@ -49,13 +49,13 @@ class Rook(pieces.Piece):
             for column_left in range(column - 1, -1, -1):
                 piece = pieces.find_piece(list_pieces, row, column_left)
                 if piece.type is None:
-                    self.available_move.append([row, column_left])
+                    self.available_moves.append([row, column_left])
                 elif self.color is not piece.color:  # ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
-                    self.available_move.append([row, column_left])
+                    self.available_moves.append([row, column_left])
                     break
                 elif self.color is piece.color:
                     break
 
 
-        return self.available_move
+        return self.available_moves
 
