607f754c8a1f6acc671264329a799362c05f247c
PRENOM NOM <EMAIL@DOMAINE>
2022-10-08 20:05:53 +0200

    correction rook.py

--- chess_game/rook.py
+++ chess_game/rook.py
@@ -11,29 +11,30 @@ class Rook(pieces.Piece):
         row = self.position[0]
         column = self.position[1]
 
-        if row < constants.row:  # ajoute les cases au dessous de la tours tant qu'aucune autre piece n'est trouvée,
-            for row_down in range(row + 1, constants.row):
+        if row < constants.row - 1:  # ajoute les cases au dessous de la tours tant qu'aucune autre piece n'est trouvée,
+            for row_up in range(row + 1, constants.row):
+                piece = pieces.find_piece(list_pieces, row_up, column)
+                if piece.type is None:
+                    self.available_moves.append([row_up, column])
+                elif self.color is not piece.color:  # ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
+                    self.available_moves.append([row_up, column])
+                    break
+                elif self.color is piece.color :
+                    break
+
+
+        if row > 0 :  # ajoute les cases au dessus de la tours tant qu'aucune autre piece n'est trouvée
+            for row_down in range(row - 1, -1, -1):
                 piece = pieces.find_piece(list_pieces, row_down, column)
                 if piece.type is None:
                     self.available_moves.append([row_down, column])
                 elif self.color is not piece.color:# ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
                     self.available_moves.append([row_down, column])
                     break
-                elif self.color is piece.color :
-                    break
-
-        if row > 0 :  # ajoute les cases au dessus de la tours tant qu'aucune autre piece n'est trouvée
-            for row_up in range(row - 1, -1, -1):
-                piece = pieces.find_piece(list_pieces, row_up, column)
-                if piece.type is None:
-                    self.available_moves.append([row_up, column])
-                elif self.color is not piece.color:# ajoute la case si une piece adverse est trouvée puis arrete la recherche sinon arrete directement la recherche
-                    self.available_moves.append([row_up, column])
-                    break
                 elif self.color is piece.color:
                     break
 
-        if column < constants.column:  # ajoute les cases a droite de la tours tant qu'aucune autre piece n'est trouvée
+        if column < constants.column - 1:  # ajoute les cases a droite de la tours tant qu'aucune autre piece n'est trouvée
             for column_right in range(column + 1, constants.column):
                 piece = pieces.find_piece(list_pieces, row, column_right)
                 print (piece.position)
