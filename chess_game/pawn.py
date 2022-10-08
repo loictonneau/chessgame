ddfc05bc06b04b8d3b25408f20d01579fb09d866
PRENOM NOM <EMAIL@DOMAINE>
2022-10-08 15:58:30 +0200

    extraction et amelioration de la methode find_piece

--- chess_game/pawn.py
+++ chess_game/pawn.py
@@ -16,119 +16,53 @@ class Pawn(pieces.Piece):
         if self.color == "white":
             # si le pion est blanc il avance en retranchant 1 au ligne
             if row - 1 >= 0:
-                find = False
-                for pieces1 in list_pieces:
-                    if not find:
-                        for piece1 in pieces1:
-                            if piece1.position == [row - 1, column]:  # trouve la case corespondante
-                                if piece1.type is None:  # verifie que la case est vide
-                                    self.available_move.append([row - 1, column])  # ajoute la case aux coups possibles
+                piece = pieces.Piece.find_piece(self,list_pieces,row - 1,column)
+                if piece.type is None:  # verifie que la case est vide
+                    self.available_move.append([row - 1, column])  # ajoute la case aux coups possibles
 
-                                    # si le pion n'a jamais bouger il peut egalement avancer de deux cases d'un coup
-                                    if self.first_move:
-                                        find = False
-                                        for pieces2 in list_pieces:
-                                            if not find:
-                                                for piece2 in pieces2:
-                                                    # trouve la case corespondante
-                                                    if piece2.position == [row - 2, column]:
-                                                        if piece2.type is None:  # verifie que la case est vide
-                                                            # ajoute la case aux coups possibles
-                                                            self.available_move.append([row - 2, column])
-                                                            find = True
-                                                            break
-                                            elif find:
-                                                break
-                                    find = True
-                                    break
-                    elif find:
-                        break
+                    # si le pion n'a jamais bouger il peut egalement avancer de deux cases d'un coup
+                    if self.first_move:
+                        piece = pieces.Piece.find_piece(self, list_pieces, row - 2, column)
+                        if piece.type is None:  # verifie que la case est vide
+                            self.available_move.append([row - 2, column])  # ajoute la case aux coups possibles
 
-                # le pion peut manger en diagonal ( version gauche )
-                if column-1 >= 0:
-                    find = False
-                    for pieces3 in list_pieces:
-                        if not find:
-                            for piece3 in pieces3:
-                                if piece3.position == [row - 1, column - 1]:  # trouve la case corespondante
-                                    if piece3.type is not None:  # verifie qu'il y a une piece sur la case
-                                        if piece3.color is not self.color:  # verifie qu'il s'agis d'une piece adverse
-                                            # ajoute la case aux coups possibles
-                                            self.available_move.append([row - 1, column - 1])
-                                            find = True
-                                            break
+                if column-1 >= 0:# le pion peut manger en diagonal ( version gauche )
+                    piece = pieces.Piece.find_piece(self,list_pieces,row - 1,column -1)
+                    if piece.type is not None:  # verifie qu'il y a une piece sur la case
+                        if self.color is not piece.color:  # verifie qu'il s'agis d'une piece adverse
+                            self.available_move.append([row - 1, column - 1])  # ajoute la case aux coups possibles
 
-                # le pion peut manger en diagonal ( version droite )
-                if column+1 >= 0:
-                    find = False
-                    for pieces4 in list_pieces:
-                        if not find:
-                            for piece4 in pieces4:
-                                if piece4.position == [row - 1, column + 1]:  # trouve la case corespondante
-                                    if piece4.type is not None:  # verifie qu'il y a une piece sur la case
-                                        if piece4.color is not self.color:  # verifie qu'il s'agis d'une piece adverse
-                                            # ajoute la case aux coups possibles
-                                            self.available_move.append([row - 1, column + 1])
-                                            find = True
-                                            break
+                if column+1 >= 0: # le pion peut manger en diagonal ( version droite )
+                    piece = pieces.Piece.find_piece(self,list_pieces,row - 1,column + 1)
+                    if piece.position == [row - 1, column + 1]:  # trouve la case corespondante
+                        if piece.type is not None:  # verifie qu'il y a une piece sur la case
+                            if self.color is not piece.color:  # verifie qu'il s'agis d'une piece adverse
+                                self.available_move.append([row - 1, column + 1])# ajoute la case aux coups possibles
+
         if self.color == "black":
+            # si le pion est noir il avance en ajoutant 1 au ligne
+            if row + 1 >= 0:
+                piece = pieces.Piece.find_piece(self, list_pieces, row + 1, column)
+                if piece.type is None:  # verifie que la case est vide
+                    self.available_move.append([row + 1, column])  # ajoute la case aux coups possibles
 
-            # si le pion est noir il avance en additionnant 1 au ligne
-            if row + 1 < constants.row:
-                find = False
-                for pieces5 in list_pieces:
-                    if not find:
-                        for piece5 in pieces5:
-                            if piece5.position == [row + 1, column]:  # trouve la case corespondante
-                                if piece5.type is None:  # verifie que la case est vide
-                                    self.available_move.append([row + 1, column])  # ajoute la case aux coups possibles
+                    # si le pion n'a jamais bouger il peut egalement avancer de deux cases d'un coup
+                    if self.first_move:
+                        piece = pieces.Piece.find_piece(self, list_pieces, row + 2, column)
+                        if piece.type is None:  # verifie que la case est vide
+                            self.available_move.append([row + 2, column])  # ajoute la case aux coups possibles
 
-                                    # si le pion n'a jamais bouger il peut egalement avancer de deux case d'un coup
-                                    if self.first_move:
-                                        find = False
-                                        for pieces6 in list_pieces:
-                                            if not find:
-                                                for piece6 in pieces6:
-                                                    # trouve la case corespondante
-                                                    if piece6.position == [row + 2, column]:
-                                                        if piece6.type is None:  # verifie que la case est vide
-                                                            # ajoute la case aux coups possibles
-                                                            self.available_move.append([row + 2, column])
-                                                            find = True
-                                                            break
-                                            elif find:
-                                                break
-                                    find = True
-                                    break
-                    elif find:
-                        break
+                if column - 1 >= 0:  # le pion peut manger en diagonal ( version gauche )
+                    piece = pieces.Piece.find_piece(self, list_pieces, row + 1, column - 1)
+                    if piece.type is not None:  # verifie qu'il y a une piece sur la case
+                        if self.color is not piece.color:  # verifie qu'il s'agis d'une piece adverse
+                            self.available_move.append([row + 1, column - 1])  # ajoute la case aux coups possibles
 
-                # le pion peut manger en diagonal ( version gauche )
-                if column-1 >= 0:
-                    find = False
-                    for pieces7 in list_pieces:
-                        if not find:
-                            for piece7 in pieces7:
-                                if piece7.position == [row + 1, column - 1]:  # trouve la case corespondante
-                                    if piece7.type is not None:  # verifie qu'il y a une piece sur la case
-                                        if piece7.color is not self.color:  # verifie qu'il s'agis d'une piece adverse
-                                            # ajoute la case aux coups possibles
-                                            self.available_move.append([row + 1, column - 1])
-                                            find = True
-                                            break
-
-                # le pion peut manger en diagonal ( version droite )
-                if column+1 >= 0:
-                    find = False
-                    for pieces8 in list_pieces:
-                        if not find:
-                            for piece in pieces8:
-                                if piece.position == [row + 1, column + 1]:  # trouve la case corespondante
-                                    if piece.type is not None:  # verifie qu'il y a une piece sur la case
-                                        if piece.color is not self.color:  # verifie qu'il s'agis d'une piece adverse
-                                            # ajoute la case aux coups possibles
-                                            self.available_move.append([row + 1, column + 1])
-                                            find = True
-                                            break
+                if column + 1 >= 0:  # le pion peut manger en diagonal ( version droite )
+                    piece = pieces.Piece.find_piece(self, list_pieces, row + 1, column + 1)
+                    if piece.position == [row - 1, column + 1]:  # trouve la case corespondante
+                        if piece.type is not None:  # verifie qu'il y a une piece sur la case
+                            if self.color is not piece.color:  # verifie qu'il s'agis d'une piece adverse
+                                self.available_move.append([row + 1, column + 1])  # ajoute la case aux coups possibles
 
         return self.available_move
--- chess_game/pieces.py
+++ chess_game/pieces.py
@@ -8,3 +8,14 @@ class Piece:
     def clear_available_move(self):
         if len(self.available_move) > 0:
             self.available_move = []
+
+    def find_piece(self,list_pieces,row,column):
+        try :
+            for pieces in list_pieces:
+                for piece in pieces:
+                    if piece.position == [row, column]:
+                        raise StopIteration
+        except StopIteration:
+            return piece
+
+
