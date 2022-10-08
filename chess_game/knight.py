d98f385199970dab2549a29088a0aea871e925e2
PRENOM NOM <EMAIL@DOMAINE>
2022-10-08 19:20:42 +0200

    creation du mouvement du cavalier

--- chess_game/knight.py
+++ chess_game/knight.py
@@ -1,7 +1,57 @@
-from . import pieces
+from . import pieces,constants
 
 
 class Knight(pieces.Piece):
 
     def __init__(self, piece_type, color, position):
         super().__init__(piece_type, color, position)
+
+    def get_available_moves(self, list_pieces):  # creer une liste avec tout les coups possibles du cavalier
+        self.clear_available_moves()
+        row = self.position[0]
+        column = self.position[1]
+
+        # le cavalier peut bouger sur 8 case differente
+        if row - 2 >= 0 and column + 1 < constants.column:
+            piece = pieces.find_piece(list_pieces,row-2,column+1)
+            if self.color is not piece.color:
+                self.available_moves.append([row-2,column+1])
+
+        if row - 2 >= 0 and column - 1 >= 0:
+            piece = pieces.find_piece(list_pieces, row - 2, column - 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 2, column - 1])
+
+        if row + 2 < constants.row and column + 1 < constants.column:
+            piece = pieces.find_piece(list_pieces, row + 2, column + 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 2, column + 1])
+
+        if row + 2 < constants.row and column - 1 >= 0:
+            piece = pieces.find_piece(list_pieces, row + 2, column - 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 2, column - 1])
+
+        if row - 1 >= 0 and column + 2 < constants.column:
+            piece = pieces.find_piece(list_pieces, row - 1, column + 2)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 1, column + 2])
+
+        if row - 1 >= 0 and column -2 >= 0:
+            piece = pieces.find_piece(list_pieces, row - 1, column - 2)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 1, column - 2])
+
+        if row + 1 < constants.row and column + 2 < constants.column:
+            piece = pieces.find_piece(list_pieces, row + 1, column + 2)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 1, column + 2])
+
+        if row + 1 < constants.row and column -2 >= 0:
+            piece = pieces.find_piece(list_pieces, row + 1, column - 2)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 1, column - 2])
+
+
+        return self.available_moves
+
