b54b07f1db26288f27d04df1e9a17bccab4e7861
PRENOM NOM <EMAIL@DOMAINE>
2022-10-09 00:19:06 +0200

    correction des deplacement de bishop.py

--- chess_game/bishop.py
+++ chess_game/bishop.py
@@ -14,31 +14,31 @@ class Bishop(pieces.Piece):
         if row + 1 < constants.row + 1 and column + 1 < constants.column + 1:  # ajoute les cases de la diagonal N.E. du fou tant qu'aucune autre piece n'est trouvée
             for row_up in range(row + 1, constants.row):
                 for column_rignt in range(column + 1, constants.column):
-                    if row_up == column_rignt:
+                    if abs(row_up - row) == abs(column_rignt - column):
                         piece = pieces.find_piece(list_pieces, row_up, column_rignt)
                         if self.color != piece.color:
                             self.available_moves.append([row_up, column_rignt])
 
         if row - 1 >= 0 and column + 1 < constants.column + 1:  # ajoute les cases de la diagonal S.E. du fou tant qu'aucune autre piece n'est trouvée
-            for row_down in range(row - 1, constants.row):
+            for row_down in range(row -1,-1,-1):
                 for column_rignt in range(column + 1, constants.column):
-                    if row_down == column_rignt:
+                    if abs(row_down - row) == abs(column_rignt - column):
                         piece = pieces.find_piece(list_pieces, row_down, column_rignt)
                         if self.color != piece.color:
                             self.available_moves.append([row_down, column_rignt])
 
         if row - 1 >= 0 and column - 1 >= 0:  # ajoute les cases de la diagonal S.O. du fou tant qu'aucune autre piece n'est trouvée
-            for row_down in range(row - 1, constants.row):
-                for column_rignt in range(column - 1, constants.column):
-                    if row_down == column_rignt:
+            for row_down in range(row - 1, -1, -1):
+                for column_rignt in range(column - 1,-1, -1):
+                    if abs(row_down - row) == abs(column_rignt - column):
                         piece = pieces.find_piece(list_pieces, row_down, column_rignt)
                         if self.color != piece.color:
                             self.available_moves.append([row_down, column_rignt])
 
         if row + 1 < constants.row + 1 and column - 1 >= 0:  # ajoute les cases de la diagonal S.O. du fou tant qu'aucune autre piece n'est trouvée
             for row_down in range(row - 1, constants.row):
-                for column_rignt in range(column - 1, constants.column):
-                    if row_down == column_rignt:
+                for column_rignt in range(column - 1, -1, -1):
+                    if abs(row_down - row) == abs(column_rignt - column):
                         piece = pieces.find_piece(list_pieces, row_down, column_rignt)
                         if self.color != piece.color:
                             self.available_moves.append([row_down, column_rignt])
