e8246e0ceb55c58ad44b09b70340ee27b056248e
PRENOM NOM <EMAIL@DOMAINE>
2022-10-10 21:58:15 +0200

    correction du mouvement de knight.py

--- chess_game/knight.py
+++ chess_game/knight.py
@@ -12,42 +12,42 @@ class Knight(pieces.Piece):
         column = self.position[1]
 
         # le cavalier peut bouger sur 8 case differente
-        if row - 2 >= 0 and column + 1 < constants.column - 1:
-            piece = game.find_piece(list_pieces, row - 2, column + 1)
-            if self.color is not piece.color:
-                self.available_moves.append([row - 2, column + 1])
-
-        if row - 2 >= 0 and column - 1 >= 0:
-            piece = game.find_piece(list_pieces, row - 2, column - 1)
-            if self.color is not piece.color:
-                self.available_moves.append([row - 2, column - 1])
-
-        if row + 2 < constants.row - 1 and column + 1 < constants.column - 1:
-            piece = game.find_piece(list_pieces, row + 2, column + 1)
-            if self.color is not piece.color:
-                self.available_moves.append([row + 2, column + 1])
-
-        if row + 2 < constants.row - 1 and column - 1 >= 0:
-            piece = game.find_piece(list_pieces, row + 2, column - 1)
-            if self.color is not piece.color:
-                self.available_moves.append([row + 2, column - 1])
-
-        if row - 1 >= 0 and column + 2 < constants.column - 1:
-            piece = game.find_piece(list_pieces, row - 1, column + 2)
-            if self.color is not piece.color:
-                self.available_moves.append([row - 1, column + 2])
-
-        if row - 1 >= 0 and column - 2 >= 0:
+        if row - 1 >= 0 and column - 2 >= 0:  # 2 gauche 1 haut
             piece = game.find_piece(list_pieces, row - 1, column - 2)
             if self.color is not piece.color:
                 self.available_moves.append([row - 1, column - 2])
 
-        if row + 1 < constants.row - 1 and column + 2 < constants.column - 1:
+        if row - 2 >= 0 and column - 1 >= 0: # 2 haut 1 gauche
+            piece = game.find_piece(list_pieces, row - 2, column - 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 2, column - 1])
+
+        if row - 2 >= 0 and column + 1 < constants.column:  # 2 haut 1 droite
+            piece = game.find_piece(list_pieces, row - 2, column + 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 2, column + 1])
+
+        if row - 1 >= 0 and column + 2 < constants.column:  # 2 droite 1 haut
+            piece = game.find_piece(list_pieces, row - 1, column + 2)
+            if self.color is not piece.color:
+                self.available_moves.append([row - 1, column + 2])
+
+        if row + 1 < constants.row and column + 2 < constants.column:  # 2 droite 1 bas
             piece = game.find_piece(list_pieces, row + 1, column + 2)
             if self.color is not piece.color:
                 self.available_moves.append([row + 1, column + 2])
 
-        if row + 1 < constants.row - 1 and column - 2 >= 0:
+        if row + 2 < constants.row and column + 1 < constants.column:  # 2 bas 1 droite
+            piece = game.find_piece(list_pieces, row + 2, column + 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 2, column + 1])
+
+        if row + 2 < constants.row and column - 1 >= 0:  # 2 bas 1 gauche
+            piece = game.find_piece(list_pieces, row + 2, column - 1)
+            if self.color is not piece.color:
+                self.available_moves.append([row + 2, column - 1])
+
+        if row + 1 < constants.row and column - 2 >= 0:  # 2 gauche 1 bas
             piece = game.find_piece(list_pieces, row + 1, column - 2)
             if self.color is not piece.color:
                 self.available_moves.append([row + 1, column - 2])
